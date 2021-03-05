8126c3934a1590b2460d31dfb5d1d2789c6913a4
jgalfonso <78840387+jgalfonso@users.noreply.github.com>
2021-03-05 09:35:20 +0800

    Update CertificationController.php

--- app/Http/Controllers/Admin/Services/CertificationController.php
+++ app/Http/Controllers/Admin/Services/CertificationController.php
@@ -7,25 +7,45 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
-use App\Models\Admin\Classes;
+use App\Models\Admin\Certificates;
+use App\Models\Admin\Moderations;
 
 class CertificationController extends Controller
 {
-    /**
-     * Display moderations of certificates
-     */
     public function moderations ()
     {
-        return view('admin.services.certification.moderations');
+
+        $summary = Certificates::getSummary();
+        $entries = Certificates::getEntries('New');
+
+        return view('admin.services.certification.moderations', compact('summary', 'entries'));
     }
 
-    /**
-     * Process of moderation of certificates
-     */
-    public function process ()
+    public function getEntries(Request $request)
     {
-        return view('admin.services.certification.process');
+        $data = Certificates::getEntries($request->status);
+
+        echo json_encode($data);
     }
+    
+    public function moderate($id)
+    {
+        $today = date('d/m/Y');
+        $entry = Certificates::getByID($id);
+        $certifications = Certificates::getCertificatesByAssessmentID($id);
+        $moderations = Moderations::getModerations($id);
+
+        return view('admin.services.certification.moderate', compact('today', 'entry', 'certifications', 'moderations'));
+    }
+
+    public function store(Request $request) 
+    {   
+        $request->request->add(['userID' => Auth::id()]);
+        $data = Moderations::add($request);
+
+        echo json_encode($data);
+    }
+
 
     /**
      * Display published certificates
